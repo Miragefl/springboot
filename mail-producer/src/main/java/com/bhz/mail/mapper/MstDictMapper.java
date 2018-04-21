9f582605b3366235b3a4899b44d114e10069b903
miragefl <silence_fl@yeah.net>
2018-04-21 18:02:56 +0800

    edit pom.xml

--- mail-producer/src/main/java/com/bhz/mail/mapper/MstDictMapper.java
+++ mail-producer/src/main/java/com/bhz/mail/mapper/MstDictMapper.java
@@ -1,11 +1,12 @@
 package com.bhz.mail.mapper;
 
 import com.bhz.mail.config.database.BaseMapper;
+import com.bhz.mail.config.database.ReadOnlyConnetion;
 import com.bhz.mail.entity.MstDict;
 
 import java.util.List;
 
 public interface MstDictMapper extends BaseMapper<MstDict> {
-
+	@ReadOnlyConnetion
 	List<MstDict> findById(String id);
 }
--- mail-producer/src/main/java/com/bhz/mail/service/MstDictService.java
+++ mail-producer/src/main/java/com/bhz/mail/service/MstDictService.java
@@ -1,12 +1,21 @@
 package com.bhz.mail.service;
 
+import com.bhz.mail.config.database.ReadOnlyConnetion;
+import com.bhz.mail.entity.MstDict;
 import com.bhz.mail.mapper.MstDictMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MstDictService {
 
 	@Autowired
 	private MstDictMapper mstDictMapper;
+	@ReadOnlyConnetion
+	public List<MstDict> findById(String id){
+		List<MstDict> list = mstDictMapper.findById(id);
+		return list;
+	}
 }
